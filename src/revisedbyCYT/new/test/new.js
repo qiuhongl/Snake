b28129ec47be83757c6c1656ab40264d9f552c26
zzzzcyt <46452789+zzzzcyt@users.noreply.github.com>
2019-04-03 00:30:29 -0400

    add food? and set the elimination when collide

--- src/revisedbyCYT/new/test/new.js
+++ src/revisedbyCYT/new/test/new.js
@@ -1,51 +1,67 @@
-var game = new Phaser.Game(800, 600, Phaser.CANVAS, 'gameDiv');
+
+
+var game = new Phaser.Game(1000, 600, Phaser.CANVAS, 'gameDiv');
 
 var background;
 var snake;
 var spacekey;
 var speed;
-
-var main={
+var veggies;
+var main= {
     preload: function () {
         game.load.image('snake', 'asserts/snake.png');
-        game.load.image('background','asserts/tile.png');
+        game.load.image('background', 'asserts/tile.png');
+//        game.load.image('bot','asserts/snake.png');
+        game.load.spritesheet('veggies', 'assets/energy.png', 32, 32);
     },
 
-    create:function () {
-        var wid=800;
-        var hei=600;
-        this.game.world.setBounds(-wid, -hei, wid*2, hei*2);
-        background = game.add.tileSprite(-wid, -hei,
-            800*2, 600*2, 'background');
+    create: function () {
+        background =game.add.tileSprite(0,0,
+            1000,600, 'background');
 
-            snake= game.add.sprite(game.world.centerX,game.world.centerY+200,'snake');
-            game.physics.enable(snake,Phaser.Physics.ARCADE);
 
-            game.camera.follow(snake)
-            spacekey=game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR)
+        veggies = game.add.group();
+        veggies.enableBody = true;
+        veggies.physicsBodyType = Phaser.Physics.ARCADE;
+
+        for (var i = 0; i < 100; i++)
+        {
+            var c = veggies.create(game.world.randomX, Math.random() * 500, 'veggies', game.rnd.integerInRange(0, 36));
+            c.name = 'veg' + i;
+            c.body.immovable = true;
+        }
+        snake = game.add.sprite(game.world.centerX, game.world.centerY + 200, 'snake');
+        game.physics.enable(snake, Phaser.Physics.ARCADE);
+
+        game.camera.follow(snake)
+        spacekey = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR)
+
+
     },
-    update: function(){
-        if (spacekey.isDown){
-            speed=400;
-            if (game.input.mousePointer.isDown){
+    update: function () {
+        game.physics.arcade.overlap(snake, veggies, collisionHandler, null, this);
+        if (spacekey.isDown) {
+            speed = 400;
+            if (game.input.mousePointer.isDown) {
                 game.physics.arcade.moveToPointer(snake, speed);
+            } else {
+                snake.body.velocity.setTo(0, 0);
             }
-            else{
-                snake.body.velocity.setTo(0,0);
-            }
-        }
-        else{
-            speed=200
-            if (game.input.mousePointer.isDown){
+        } else {
+            speed = 200
+            if (game.input.mousePointer.isDown) {
                 game.physics.arcade.moveToPointer(snake, speed);
-            }
-            else{
-                snake.body.velocity.setTo(0,0);
+            } else {
+                snake.body.velocity.setTo(0, 0);
             }
         }
 
-},
+    },
 }
+function collisionHandler(snake,veg){
+    veg.kill();
+}
+
+
 game.state.add('game',main)
 game.state.start('game')
-
