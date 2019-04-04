d6640de7645eae72afb8675da0a2b5a76d8722e6
yufansun98 <47640087+yufansun98@users.noreply.github.com>
2019-04-03 22:50:27 -0400

    Update new.js

--- src/new/test/new.js
+++ src/new/test/new.js
@@ -9,11 +9,16 @@ var speed;
 var veggies;
 var length=0;
 var lengthtext;
+//
+var bot;
+//
 var main= {
     preload: function () {
         game.load.image('snake', 'asserts/snake.png');
-        game.load.image('background', 'asserts/tilesprite.jpg');
-//        game.load.image('bot','asserts/snake.png');
+        game.load.image('background', 'asserts/tile.png');
+        //
+        game.load.image('bot','asserts/snake.png');
+        //
         game.load.spritesheet('veggies', 'asserts/energy.png',32,32);
     },
 
@@ -26,7 +31,7 @@ var main= {
         veggies.enableBody = true;
         veggies.physicsBodyType = Phaser.Physics.ARCADE;
 
-        for (var i = 0; i < 130; i++)
+        for (var i = 0; i < 50; i++)
         {
             var c = veggies.create(game.world.randomX, Math.random() * 600, 'veggies', game.rnd.integerInRange(0, 36));
             c.name = 'veg' + i;
@@ -34,8 +39,11 @@ var main= {
         }
         snake = game.add.sprite(game.world.centerX, game.world.centerY + 200, 'snake');
         game.physics.enable(snake, Phaser.Physics.ARCADE);
-        lengthtext=game.add.text(1300,650,'length:',{font:'24px Arial',})
-
+        lengthtext=game.add.text(1300,650,'length:',{font:'24px Arial',});
+        //
+        bot = game.add.sprite(game.world.centerX, game.world.centerY, 'bot');
+        game.physics.enable(bot, Phaser.Physics.ARCADE)
+        //
         game.camera.follow(snake)
         spacekey = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR)
 
@@ -43,6 +51,7 @@ var main= {
     },
     update: function () {
         game.physics.arcade.overlap(snake, veggies, collisionHandler, null, this);
+        game.physics.arcade.overlap(snake, bot, collisionHandler1, null, this)
         if (spacekey.isDown) {
             speed = 400;
             if (game.input.mousePointer.isDown) {
@@ -51,7 +60,7 @@ var main= {
                 snake.body.velocity.setTo(0, 0);
             }
         } else {
-            speed = 200
+            speed = 200;
             if (game.input.mousePointer.isDown) {
                 game.physics.arcade.moveToPointer(snake, speed);
             } else {
@@ -59,14 +68,33 @@ var main= {
             }
         }
         lengthtext.text ='length:'+length;
-    },
-}
+    }
+};
+
 function collisionHandler(snake,veg){
     veg.kill();
     length +=1;
 }
 
+//
+function collisionHandler1(snake,bot){
+    bot.kill();
+    length +=1;
+}
 
-game.state.add('game',main)
-game.state.start('game')
+function rank(input){
+    var score = 0;
+    var winner = "";
+    for (var i in input){
+        if (input[i] >= score){
+            score = input[i];
+            winner = i
+        }
+    }
+    return winner
+}
+//
+
+game.state.add('game',main);
+game.state.start('game');
 
